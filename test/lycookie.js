832b255dc9666223c759bb7ea7be3ebdce667677
Serein1202 <xhd0504@outlook.com>
2025-11-19 20:14:23 +0800

    test

--- test/lycookie.js
+++ test/lycookie.js
@@ -1,16 +1,24 @@
-// 自动抓取领克签到 Cookie
-const ck = $request.headers["Cookie"] || $request.headers["cookie"];
-
-if (ck) {
-  const old = $persist.read("lynk_cookie");
-  if (old !== ck) {
-    $persist.store("lynk_cookie", ck);
-    $notification.post("领克 Cookie 已更新 ✅", "", ck);
-  } else {
-    $notification.post("领克 App 已打开 ℹ️", "", "Cookie 未变化");
-  }
-} else {
-  $notification.post("未捕获到 Cookie ❌", "", "请打开领克 App 的签到页面");
+const lk = new ToolKit(`阿里云盘签到`, `AliYunPanCheckIn`, {"httpApi": "ffff@10.0.0.19:6166"})
+function getCookie() {
+    // 判断是否是领克签到接口
+    if (lk.isGetCookie(/\/user\/sign\/sign\/info/)) {
+        lk.log(`开始获取领克Cookie`);
+        let data = lk.getResponseBody();
+        // lk.log(`获取到的Cookie：${data}`)
+        try {
+            // 这里我们直接获取 Cookie 而不是 JSON 内容
+            let cookie = lk.getRequestHeader("Cookie") || lk.getRequestHeader("cookie");
+            if (cookie) {
+                lk.setVal(lynkCookieKey, cookie);
+                lk.appendNotifyInfo('🎉成功获取领克签到Cookie，可以关闭相应脚本');
+            } else {
+                lk.execFail();
+                lk.appendNotifyInfo('❌获取领克Cookie失败，请稍后再试');
+            }
+        } catch (e) {
+            lk.execFail();
+            lk.appendNotifyInfo('❌获取领克Cookie失败');
+        }
+        lk.msg('');
+    }
 }
-
-$done({});
