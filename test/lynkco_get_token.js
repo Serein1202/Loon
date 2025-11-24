468ff30ad652ae373c397364f52f7e5c2401145a
Serein1202 <xhd0504@outlook.com>
2025-11-24 20:42:55 +0800

    test

--- test/lynkco_get_token.js
+++ test/lynkco_get_token.js
@@ -6,9 +6,9 @@ let token = $request.headers["token"];
 
 if (token) {
   $persistentStore.write(token, "lynkco_token");
-  $notification.post("领克 Token", "获取成功", token);
+  $notification.post("领克 Token", "✔ 获取成功", token);
 } else {
-  $notification.post("领克 Token", "获取失败", "未找到 token 字段");
+  $notification.post("领克 Token", "❌ 获取失败", "未找到 token 字段");
 }
 
 $done({});
