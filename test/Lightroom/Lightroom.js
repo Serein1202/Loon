12bbcddf5008b708d26702450d25f9a1dbb47acd
Serein1202 <xhd0504@outlook.com>
2025-12-17 16:38:15 +0800

    2025-12-17 16:15:00

--- test/Lightroom/Lightroom.js
+++ test/Lightroom/Lightroom.js
@@ -1,12 +1,19 @@
 body = $response.body.replace(/while.{7}\n/, "");
 let obj = JSON.parse(body);
-obj.entitlement.status="subscriber";
-obj.entitlement.storage={
-      "used": 0,
-      "limit": 1154487209165,
-      "display_limit": 1099511627776,
-      "warn": 992137445376
-    };
-obj.avatar.placeholder=true;
-body = JSON.stringify(obj); 
-$done({body});
+obj.entitlement.status = "subscriber";
+obj.current_subs = {
+    "product_id": "lightroom",
+    "store": "adobe",
+    "purchase_date": "2025-12-17T08:26:17.664056Z",
+    "sao": {
+        "inpkg_LRMC": "1"
+    }
+};
+obj.entitlement.storage = {
+    "used": 4060414,
+    "limit": 112742891520,
+    "display_limit": 107374182400,
+    "warn": 85899345920
+};
+body = JSON.stringify(obj);
+$done({ body });
