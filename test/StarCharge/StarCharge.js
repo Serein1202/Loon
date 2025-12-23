6095a6c9aba9602ea21cc50e7d6098e1213e9b0f
Serein1202 <xhd0504@outlook.com>
2025-12-23 19:08:23 +0800

    2025-12-23 19:30:00

--- test/StarCharge/StarCharge.js
+++ test/StarCharge/StarCharge.js
@@ -1,11 +1,17 @@
 if (!$response.body) {
-  $done({});
+    $done({});
 }
 let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 if (url.includes("/apph5/webApiV2/car/user/function")) {
-    obj.data={};
+    obj.data = {
+        "adVO": [],
+        "menuVO": []
+    };
+}
+if (url.includes("/apph5/webApiV2/system/systemInfo")) {
+    obj.data.aiChatSwitch = 0;
 }
 body = JSON.stringify(obj);
 $done({ body });
