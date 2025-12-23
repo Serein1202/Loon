721b09393078afd8426d3ea3fe91fdeb7c1fd030
Serein1202 <xhd0504@outlook.com>
2025-12-23 18:55:37 +0800

    2025-12-23 19:30:00

---
+++ test/StarCharge/StarCharge.js
@@ -0,0 +1,11 @@
+if (!$response.body) {
+  $done({});
+}
+let url = $request.url;
+let body = $response.body;
+let obj = JSON.parse(body);
+if (url.includes("/apph5/webApiV2/car/user/function")) {
+    obj.data={};
+}
+body = JSON.stringify(obj);
+$done({ body });
