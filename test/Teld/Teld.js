b50f84995df6fe79e8ce0ba08c89a307cb856383
Serein1202 <xhd0504@outlook.com>
2025-12-23 15:24:28 +0800

    2025-12-23 15:30:00

--- test/Teld/Teld.js
+++ test/Teld/Teld.js
@@ -6,30 +6,30 @@ let body = $response.body;
 let obj = JSON.parse(body);
 if (url.includes("api/invoke?SID=CMS-GetMergePromoEntry")) {
     obj.data=[];
-    // obj.statr=0;
+    // obj.state=0;
 }
 // if (url.includes("api/invoke?SID=CMS-GetFindTabsNew")) {
 //     obj.data=[];
-//     // obj.statr=0;
+//     // obj.state=0;
 // }
 if (url.includes("api/invoke?SID=CMS-GetConfigRelease")) {
     obj.data.ConfigContent={};
-    // obj.statr=0;
+    // obj.state=0;
 }
 if (url.includes("api/invoke?SID=ProSrv-GetInviteNewActivityInfo")) {
     obj.data={};
-    // obj.statr=0;
+    // obj.state=0;
 }
 if (url.includes("api/invoke?SID=CMS-GetSysReleaseWithLatest")) {
     obj.data={};
-    // obj.statr=0;
+    // obj.state=0;
 }
 if (url.includes("api/invoke?SID=CMS-GetCityChangedResources")) {
     obj.data=[];
-    // obj.statr=0;
+    // obj.state=0;
 }
 if (url.includes("api/invoke?SID=App-CheckHealthStateV2")) {
-    obj.statr=0;
+    obj.state=0;
 }
 body = JSON.stringify(obj);
 $done({ body });
