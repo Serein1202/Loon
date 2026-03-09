7abde383d3c0d77116bcaf99c4a3f10444b68191
Serein1202 <xhd0504@outlook.com>
2026-03-09 17:56:39 +0800

    update Oil.js

--- Scripts/Oil/Oil.js
+++ Scripts/Oil/Oil.js
@@ -19,69 +19,50 @@ var params = {
 };
 
 $httpClient.get(params, function(error,response,data) {
-    if (error) {
-      console.log(`解析油价信息失败`);
-      done({});
+    if (error || !data) {
+    console.log("油价获取失败");
+    $done();
+    return;
+}
+
+let prices = {};
+
+let reg = /(92|95|98|0)号[^0-9]*([\d\.]+)\(元\)/g;
+
+let match;
+
+while ((match = reg.exec(data)) !== null) {
+
+    let type = match[1];
+    let price = match[2];
+
+    if (type == "0") {
+        prices["柴油"] = price;
     } else {
-      const reg_price =
-        /<dl>[\s\S]+?<dt>(.*油)<\/dt>[\s\S]+?<dd>(.*)\(元\)<\/dd>/gm;
-
-      var prices = [];
-      var m = null;
-
-      while ((m = reg_price.exec(data)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === reg_price.lastIndex) {
-          reg_price.lastIndex++;
-        }
-
-        prices.push({
-          name: m[1],
-          value: `${m[2]} 元/L`,
-        });
-      }
-
-      // 解析油价调整趋势
-      var adjust_date = "";
-      var adjust_trend = "";
-      var adjust_value = "";
-
-      const reg_adjust_tips =
-        /<div class="tishi"> <span>(.*)<\/span><br\/>([\s\S]+?)<br\/>/;
-      const adjust_tips_match = data.match(reg_adjust_tips);
-
-      if (adjust_tips_match && adjust_tips_match.length === 3) {
-        adjust_date = adjust_tips_match[1].split("价")[1].slice(0, -2);
-
-        adjust_value = adjust_tips_match[2];
-        adjust_trend =
-          adjust_value.indexOf("下调") > -1 || adjust_value.indexOf("下跌") > -1
-            ? "↓"
-            : "↑";
-
-        const adjust_value_re = /([\d\.]+)元\/升-([\d\.]+)元\/升/;
-        const adjust_value_re2 = /[\d\.]+元\/吨/;
-        const adjust_value_match = adjust_value.match(adjust_value_re);
-
-        if (adjust_value_match && adjust_value_match.length === 3) {
-          adjust_value = `${adjust_value_match[1]}-${adjust_value_match[2]}元/L`;
-        } else {
-          const adjust_value_match2 = adjust_value.match(adjust_value_re2);
-
-          if (adjust_value_match2) {
-            adjust_value = adjust_value_match2[0];
-          }
-        }
-      }
-
-      const friendly_tips = `下次${adjust_date}调整 ${adjust_trend} ${adjust_value}`;
-
-      if (prices.length !== 4) {
-        console.log( `解析油价信息失败`);
-        done();
-      } else {
-      const query_addr = `http://m.qiyoujiage.com/${region}.shtml`; 
-        $done($notification.post("实时油价信息", `${friendly_tips}`, `${prices[0].name}  ${prices[0].value}\n${prices[1].name}  ${prices[1].value}\n${prices[2].name}  ${prices[2].value}\n${prices[3].name}  ${prices[3].value}`, query_addr));
-      }
+        prices[type] = price;
     }
-  });
+}
+
+if (!prices["92"]) {
+    console.log("解析油价信息失败");
+    $done();
+    return;
+}
+
+let text =
+`92号汽油  ${prices["92"]} 元/L
+95号汽油  ${prices["95"]} 元/L
+98号汽油  ${prices["98"]} 元/L
+0号柴油  ${prices["柴油"]} 元/L`;
+
+$notification.post(
+"杭州今日油价",
+"",
+text,
+{url:url}
+);
+// console.log("data", data);
+    $done();
+});
+
+
